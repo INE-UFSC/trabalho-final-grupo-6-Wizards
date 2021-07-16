081eab445b0abde2d26143cf8d29aca28144323d
LucasYuki <lucasyuki@yahoo.com.br>
2021-07-16 18:32:45 -0300

    Estrutura da classe Ator

---
+++ Actor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul 16 18:02:51 2021
+
+@author: 
+    Guilherme Barcellos Neves
+    Lucas Yuki Imamura
+    Vitor Hugo Homem Marzarotto
+"""
